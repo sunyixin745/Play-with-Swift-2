f84c66bae627e4093a950cdacc82b67ee17e96fb
liuyubobobo <liuyubobobo@gmail.com>
2014-01-01 12:00:25 +0800

    Chapter 2 structure changed.

--- 02-Basics/03-1-Float-and-Double.playground/Contents.swift
+++ 02-Basics/03-1-Float-and-Double.playground/Contents.swift
@@ -4,8 +4,8 @@ import UIKit
 
 var x = 3.1415926
 
-var y: Float = 3.1415926
-var z: Double = 3.1415926
+var imFloat: Float = 3.1415926
+var imDouble: Double = 3.1415926
 
 
 // 使用科学计数法标示
@@ -15,10 +15,3 @@ var b = 1.25e-10
 
 // 使用_标识位置
 var c = 1_000.000_000_1
-
-
-// CGFloat
-var red: CGFloat   = 0.1
-var green: CGFloat = 0.5
-var blue: CGFloat =  0.3
-UIColor(red: red, green: green, blue: blue, alpha: 1.0)
--- 02-Basics/03-2-Conversion.playground/Contents.swift
+++ 02-Basics/03-2-Conversion.playground/Contents.swift
@@ -10,14 +10,27 @@ let y: UInt8  = 20
 let imUInt16 = x + UInt16(y)
 let imUInt8 = UInt8(x) + y
 
+let a:Double = 3.0
+let b:Float = 0.3
+
+Float(a) + b
+a + Double(b)
+
 
 // 3可以理解成一个浮点数
-let a:Float = 3
+let w:Float = 3
 // 3.0不可以理解成一个整数
-//let b:Int   = 3.0
+//let v:Int   = 3.0
 
 
 // 整数和小数之间也不可以自动类型转换
 let integer = 3
 let fraction = 0.1415926
 let pi = Double(integer) + fraction
+
+
+// CGFloat
+var red: CGFloat   = 0.1
+var green: CGFloat = 0.5
+var blue: CGFloat =  0.3
+UIColor(red: red, green: green, blue: blue, alpha: 1.0)
