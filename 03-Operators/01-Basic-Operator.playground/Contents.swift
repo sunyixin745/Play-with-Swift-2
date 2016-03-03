b332211f28eccb6f0a229cd1dab35f43cb2dc925
liuyubobobo <liuyubobobo@gmail.com>
2016-03-04 02:48:15 +0800

    Chapter 03 prepared.

---
+++ 03-Operators/01-Basic-Operator.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//赋值运算符
+var a = 3
+a = 2
+// 赋值语句没有返回值
+//if a = 1{
+//    print("a = 1")
+//}
+
+
+//数学运算符
+var x = 10
+var y = 3
+var z = 0
+
+x + y
+x - y
+x * y
+x / y
+x % y
+
+// 对于 / 和 %，除数不能为0
+//x / z
+//x % z
+
+let u = 2.5
+let v = 1.2
+u%v
+let w = 3.0
+w%v
+
+// 一元运算符
+var xx = +x
+var yy = -y
+
+// ++ 和 --
+x++
+++x
+
+// += , %=
+x += 2
+x %= 2
+
+
