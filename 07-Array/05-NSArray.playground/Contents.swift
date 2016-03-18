5d6b3fe08a6efc20a29e50149f5c024725af8b67
liuyubobobo <liuyubobobo@gmail.com>
2016-03-18 21:11:51 +0800

    Chapter 07 updated.

--- 07-Array/05-NSArray.playground/Contents.swift
+++ 07-Array/05-NSArray.playground/Contents.swift
@@ -8,6 +8,8 @@ var array2 = [1,2,3,4,5] as NSArray
 
 var array3: NSArray = [1,"hello"]
 
-var array4 = [1,"hello"]
+var array4: [NSObject] = [1,"hello"]
 
+// NextStep - NS
+// UserInterface - UI
 
