9cfd8babdadc62291eb21a63bcdd09ec301baea7
liuyubobobo <liuyubobobo@gmail.com>
2016-03-23 16:43:50 +0800

    01-3 update to Swift 2.2

--- 01-Introduction/03-Play-with-UIKit.playground/Contents.swift
+++ 01-Introduction/03-Play-with-UIKit.playground/Contents.swift
@@ -10,10 +10,14 @@ view.backgroundColor = UIColor.orangeColor()
 let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
 button.center = view.center
 
+// Swift 2.2中，Playground中的UI控件支持交互了。简单为button在Highlighted的状态下设置另外一个颜色，就可以点击看到效果了:)
 button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+button.setTitleColor(UIColor.blueColor(), forState: .Highlighted)
+
 button.setTitle("Click Me :)", forState: .Normal)
 
 view.addSubview(button)
+
 
 XCPlaygroundPage.currentPage.liveView = view
 
