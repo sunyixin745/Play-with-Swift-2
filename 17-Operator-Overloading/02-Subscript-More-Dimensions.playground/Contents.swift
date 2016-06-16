c49c1d4cba761306abaea525ce2b5a987188a9cb
liuyubobobo <liuyubobobo@gmail.com>
2016-06-16 15:33:37 +0800

    17-2 add new example to explain the difference in [x,y] and [x][y] for subscript.

--- 17-Operator-Overloading/02-Subscript-More-Dimensions.playground/Contents.swift
+++ 17-Operator-Overloading/02-Subscript-More-Dimensions.playground/Contents.swift
@@ -19,7 +19,7 @@ struct Matrix{
         }
     }
     
-    subscript(x:Int,y:Int) -> Double{
+    subscript(x: Int, y: Int) -> Double{
         
         get{
             assert( x >= 0 && x < r && y >= 0 && y < c , "Index Out of Range")
@@ -37,7 +37,49 @@ var m = Matrix(row: 2, col: 2)
 //m[2,2]
 m[1,1]
 
+
 // 注意区分,不是m[1][1]
+struct Room{
+    let info: String
+}
+
+struct Floor{
+    let level: Int
+    var rooms: [Room]
+    
+    subscript(index: Int) -> Room{
+        assert( index >= 1 && index <= rooms.count , "Room Index Out of Range")
+        return rooms[index-1]
+    }
+}
+
+class Building{
+    let levelNumber: Int
+    var floors: [Floor]
+    
+    init(levelNumber: Int, roomNumber: Int){
+        self.levelNumber = levelNumber
+        floors = [Floor]()
+        for l in 1...levelNumber{
+            var floor = Floor(level: l, rooms: [])
+            for _ in 1...roomNumber{
+                let roomInfo = String(random())
+                let room = Room(info: roomInfo)
+                floor.rooms.append(room)
+            }
+            floors.append(floor)
+        }
+    }
+    
+    subscript(index:Int) -> Floor{
+        assert( index >= 1 && index <= levelNumber , "Floor Index Out of Range")
+        return floors[index-1]
+    }
+}
+
+let building = Building(levelNumber: 10, roomNumber: 20)
+building[3][7].info
+
 
 // 重载[]，
 // 实现魔方
